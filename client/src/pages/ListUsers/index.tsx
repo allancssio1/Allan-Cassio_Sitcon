0cfe6e4d63166cd2436372d41b004368a7983d1a
allancassio <allan.cassio1@gmail.com>
2023-08-24 23:25:00 -0300

    create table of users list

--- client/src/pages/ListUsers/index.tsx
+++ client/src/pages/ListUsers/index.tsx
@@ -11,6 +11,96 @@ export const ListUsers = () => {
           </i>
           <input type="text" id="searchUser" placeholder="Pesquisar" />
         </div>
+        <section className="tableUsers">
+          <div className="headerTable itemsTable">
+            <div className="itemTable">Paciente</div>
+            <div className="itemTable">Nascimento</div>
+            <div className="itemTable">CPF</div>
+            <div className="itemTable">Ação</div>
+          </div>
+          <div className="contentTable ">
+            <div className="itemsTable colorOne">
+              <div className="itemTable">ALlan Cássio</div>
+              <div className="itemTable">26/01/1991</div>
+              <div className="itemTable">044.997.303-41</div>
+              <div className="itemTable">
+                <button className="buttonAction">Prosseguir</button>
+              </div>
+            </div>
+            <div className="itemsTable colorTwo">
+              <div className="itemTable">ALlan Cássio</div>
+              <div className="itemTable">26/01/1991</div>
+              <div className="itemTable">044.997.303-41</div>
+              <div className="itemTable">
+                <button className="buttonAction">Prosseguir</button>
+              </div>
+            </div>
+            <div className="itemsTable colorOne">
+              <div className="itemTable">ALlan Cássio</div>
+              <div className="itemTable">26/01/1991</div>
+              <div className="itemTable">044.997.303-41</div>
+              <div className="itemTable">
+                <button className="buttonAction">Prosseguir</button>
+              </div>
+            </div>
+            <div className="itemsTable colorTwo">
+              <div className="itemTable">ALlan Cássio</div>
+              <div className="itemTable">26/01/1991</div>
+              <div className="itemTable">044.997.303-41</div>
+              <div className="itemTable">
+                <button className="buttonAction">Prosseguir</button>
+              </div>
+            </div>
+            <div className="itemsTable colorOne">
+              <div className="itemTable">ALlan Cássio</div>
+              <div className="itemTable">26/01/1991</div>
+              <div className="itemTable">044.997.303-41</div>
+              <div className="itemTable">
+                <button className="buttonAction">Prosseguir</button>
+              </div>
+            </div>
+            <div className="itemsTable colorTwo">
+              <div className="itemTable">ALlan Cássio</div>
+              <div className="itemTable">26/01/1991</div>
+              <div className="itemTable">044.997.303-41</div>
+              <div className="itemTable">
+                <button className="buttonAction">Prosseguir</button>
+              </div>
+            </div>
+            <div className="itemsTable colorOne">
+              <div className="itemTable">ALlan Cássio</div>
+              <div className="itemTable">26/01/1991</div>
+              <div className="itemTable">044.997.303-41</div>
+              <div className="itemTable">
+                <button className="buttonAction">Prosseguir</button>
+              </div>
+            </div>
+            <div className="itemsTable colorTwo">
+              <div className="itemTable">ALlan Cássio</div>
+              <div className="itemTable">26/01/1991</div>
+              <div className="itemTable">044.997.303-41</div>
+              <div className="itemTable">
+                <button className="buttonAction">Prosseguir</button>
+              </div>
+            </div>
+            <div className="itemsTable colorOne">
+              <div className="itemTable">ALlan Cássio</div>
+              <div className="itemTable">26/01/1991</div>
+              <div className="itemTable">044.997.303-41</div>
+              <div className="itemTable">
+                <button className="buttonAction">Prosseguir</button>
+              </div>
+            </div>
+            <div className="itemsTable colorTwo">
+              <div className="itemTable">ALlan Cássio</div>
+              <div className="itemTable">26/01/1991</div>
+              <div className="itemTable">044.997.303-41</div>
+              <div className="itemTable">
+                <button className="buttonAction">Prosseguir</button>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   )
