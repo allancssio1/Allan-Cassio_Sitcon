fd01b34da251a845c0179248c4439bbdebc23b9f
allancassio <allan.cassio1@gmail.com>
2023-08-28 21:54:32 -0300

    modified file with data and script withdrawal

--- api/prisma/migrations/20230828011451_create_migation/migration.sql
+++ api/prisma/migrations/20230828011451_create_migation/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "patients" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "birthDate" DATETIME NOT NULL,
     "cpf" TEXT NOT NULL,
@@ -9,32 +9,32 @@ CREATE TABLE "patients" (
 
 -- CreateTable
 CREATE TABLE "professionals" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "status" BOOLEAN NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "requestTypes" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "description" TEXT NOT NULL,
     "status" BOOLEAN NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "procedures" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "description" TEXT,
     "status" BOOLEAN NOT NULL,
-    "requestTypeId" TEXT NOT NULL,
+    "requestTypeId" INTEGER NOT NULL,
     CONSTRAINT "procedures_requestTypeId_fkey" FOREIGN KEY ("requestTypeId") REFERENCES "requestTypes" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "attendants" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "procedureId" TEXT NOT NULL,
-    "professionalId" TEXT NOT NULL,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "procedureId" INTEGER NOT NULL,
+    "professionalId" INTEGER NOT NULL,
     CONSTRAINT "attendants_procedureId_fkey" FOREIGN KEY ("procedureId") REFERENCES "procedures" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "attendants_professionalId_fkey" FOREIGN KEY ("professionalId") REFERENCES "professionals" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express'
 import { routes } from './routes'
+import { uploadFileData } from '../uploadData'
 
 const app = express()
+
+uploadFileData()
 
 app.use(routes)
 
