5b167c8df7d3f9b91461539472349a89388676a0
allancassio <allan.cassio1@gmail.com>
2023-08-31 16:49:37 -0300

    create navegate routes

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -3,8 +3,18 @@ import './styles.css'
 export const Header = () => {
   return (
     <header>
-      <button className="clinical-request__button">Solicitação Clínica</button>
-      <button className="request-list__button">Lista de Solicitação</button>
+      <button
+        className="clinical-request__button"
+        onClick={() => (window.location.pathname = '/')}
+      >
+        Solicitação Clínica
+      </button>
+      <button
+        className="request-list__button"
+        onClick={() => (window.location.pathname = '/list/request')}
+      >
+        Lista de Solicitação
+      </button>
     </header>
   )
 }
